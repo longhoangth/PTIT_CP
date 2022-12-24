45cbfaf92d6dca99dd55b07b7716bd794d13e325
Hoàng Thành Long <longzil193@gmail.com>
2022-12-24 10:48:08 +0700

    Update NGUYENTO007.cpp

--- Competitive/NGUYENTO007.cpp
+++ Competitive/NGUYENTO007.cpp
@@ -18,22 +18,22 @@ int maxPrimeFactors(int n)
 	// Print the number of 2s that divide n
 	while (n % 2 == 0) {
 		maxPrime = 2;
-		n >>= 1; // equivalent to n /= 2
+		n /= 2; // equivalent to n /= 2
 	}
     // n must be odd at this point
 	while (n % 3 == 0) {
 		maxPrime = 3;
-		n=n/3;
+		n /= 3;
 	}
 
 	// now we have to iterate only for integers
-	// who does not have prime factor 2 and 3
+	// which does not have prime factor 2 and 3
 	for (int i = 5; i <= sqrt(n); i += 6) {
 		while (n % i == 0) {
 			maxPrime = i;
 			n = n / i;
 		}
-	while (n % (i+2) == 0) {
+		while (n % (i+2) == 0) {
 			maxPrime = i+2;
 			n = n / (i+2);
 		}
